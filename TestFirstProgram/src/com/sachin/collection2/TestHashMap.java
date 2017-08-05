ab8b71937227e8eaf2707735025c79b0a384759a
ssaxena16 <saxena.sachin89@gmail.com>
2017-08-06 01:04:30 +0530

    Add HashMap program

---
+++ TestFirstProgram/src/com/sachin/collection2/TestHashMap.java
@@ -0,0 +1,36 @@
+package com.sachin.collection2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class TestHashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Key1", "First");
+		map.put("key2", "Second");
+		map.put("key3", "Third");
+		map.put("key4", "Four");
+		map.put("key5", "Five");
+		// First Way to iterate a Map
+		for(Map.Entry<String, String> mapentry : map.entrySet())
+		{
+			System.out.println("Key   "+mapentry.getKey()+ " Value   "+mapentry.getValue());
+		}
+
+		// Second Way to iterate 
+		
+		Set<String> set = (Set)map.keySet();
+		Iterator< String> itr = set.iterator();
+		while(itr.hasNext())
+		{
+			String key = itr.next();
+			System.out.println(" Key  "+key+ "  Value "+map.get(key));
+		}
+	}
+
+}
