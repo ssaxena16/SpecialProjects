6164816db05c188766f553129160c532231a59a6
ssaxena16 <saxena.sachin89@gmail.com>
2017-08-05 23:35:07 +0530

    Sevlet Example

---
+++ ServletExampleProject/src/com/sachin/servletexample/LoginAction.java
@@ -0,0 +1,35 @@
+package com.sachin.servletexample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginAction extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String userName = req.getParameter("userName");
+		String password = req.getParameter("passWord");
+		
+		if("test".equals(userName) && "test".equals(password))
+		{
+			resp.sendRedirect("success.html");
+		}
+		else
+		{
+			PrintWriter out = resp.getWriter();
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/home.html");
+			rd.include(req, resp);
+		}
+		
+	}
+	
+
+}
