393ee3bcfa02434a4d0e25c1cbe3cf2bc4f37bf5
ssaxena16 <saxena.sachin89@gmail.com>
2017-09-24 19:47:46 +0530

    24/09

--- HelloMvc_Spring/src/main/java/com/sachin/hellospring/HelloWorldController.java
+++ HelloMvc_Spring/src/main/java/com/sachin/hellospring/HelloWorldController.java
@@ -9,21 +9,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
-	
+
 	@RequestMapping("/hello")
-	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse response) {  
+	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse response) {
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
-		if ("password".equals(password))
-		{
-			String message = "HELLO "+name;  
-			return new ModelAndView("hellopage", "message", message); 
-		}
-		else{
+		if ("password".equals(password)) {
+			String message = "HELLO " + name;
+			return new ModelAndView("hellopage", "message", message);
+		} else {
 			String erroemsg = "Sorry User or Password is wrong ";
-			return new ModelAndView("errorpage", "message", erroemsg);  
+			return new ModelAndView("errorpage", "message", erroemsg);
 		}
-        
-    }  
+
+	}
 
 }
