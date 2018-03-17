5719261dbb58d1bcae254c23b78eb450461de694
ssaxena16 <saxena.sachin89@gmail.com>
2018-03-17 17:56:47 +0530

    code for Second component

--- Angular4/Angular4-app/src/app/app.component.ts
+++ Angular4/Angular4-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello Angular Program';
+  months = ["January", "Feburary", "March", "April", "May",
+            "June", "July", "August", "September",
+            "October", "November", "December"];
+  isAvailable = true ;
 }
